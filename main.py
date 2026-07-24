d37557e3297286a4101a2d0ed4c7d72197c91148
yknottyd <ethan.bertin-prevot@epitech.eu>
2026-07-25 01:41:24 +0200

    [main] Got it working - 'pass'.

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ import numpy             as np
 import pandas            as pd
 import matplotlib.pyplot as plt
 import kagglehub
+import os
 from   PIL                     import Image
 from   sklearn.model_selection import train_test_split
 from   sklearn.preprocessing   import StandardScaler
 from   sklearn.decomposition   import PCA
+from   sklearn.linear_model    import LogisticRegression
+from   sklearn.ensemble        import RandomForestClassifier
 
 # TODO: switch out PIL for something else
+# TODO: a logger
 
 PENUMONIA = kagglehub.dataset_download("iamtapendu/rsna-pneumonia-processed-dataset")
 PENUMONIA_TRAIN_CSV    = PENUMONIA + "/stage2_train_metadata.csv"
 PNEUMONIA_TRAIN_IMAGES = PENUMONIA + "/Training/Images"
 PNEUMONIA_TRAIN_MASKS  = PENUMONIA + "/Training/Masks"
 
-IMAGE_COUNT = 1000
-IMAGE_SIZE  = (500, 500)
+IMAGE_COUNT = 4000
+IMAGE_SIZE  = (400, 400)
 
 PCA_DIMENTION_FACT = 0.1
 REDUCED_DIMENTION  = round(IMAGE_COUNT * PCA_DIMENTION_FACT)
@@ -48,7 +52,7 @@ print("\nNaNs:")
 NaNs = train_metadata.isnull().sum()
 print(NaNs[NaNs > 0], end = "\n" * 2)
 
-dataset_y = np.array(tuple(map(lambda out: [out], train_metadata["Target"]))[:IMAGE_COUNT])
+dataset_y = np.array(tuple(map(lambda out: out, train_metadata["Target"]))[:IMAGE_COUNT])
 
 def load_training_data(
         load_len: int,
@@ -77,13 +81,13 @@ def load_training_data(
     dataset_x = StandardScaler().fit_transform(dataset_x)
     print("Done.\n")
 
-    print("Reducing datset dimensionality...")
+    print("Reducing dataset dimensionality...")
     dataset_x = PCA(n_components = REDUCED_DIMENTION).fit_transform(dataset_x)
     print("Done.\n")
 
     return np.array(dataset_x)
 
-class OutdatedCachedDataset(Exception):
+class OutdatedCacheError(Exception):
     pass
 
 try:
@@ -96,16 +100,27 @@ try:
         dataset_x.shape[0] != IMAGE_COUNT or
         dataset_x.shape[1] != REDUCED_DIMENTION
     ):
-        print("Loading dataset from source due to outdated cache.")
-        raise OutdatedCachedDataset
+        raise OutdatedCacheError("Loading dataset from source due to outdated cache.")
 
-except (FileNotFoundError, OutdatedCachedDataset):
+except (FileNotFoundError, OutdatedCacheError) as e:
+
+    if isinstance(e, OutdatedCacheError):
+        print(e)
+    else:
+        print(f"{e.strerror}: '{e.filename}'.")
+
     dataset_x = load_training_data(
         IMAGE_COUNT,
         IMAGE_SIZE,
         REDUCED_DIMENTION,
     )
+
+    if not os.path.exists(".cache"):
+        os.makedirs(".cache")
+
+    print("Saving dataset to '.cache/dataset_x.npy'...")
     np.save(".cache/dataset_x", dataset_x)
+    print("Done.\n")
 
 train_x, test_x, train_y, test_y = train_test_split(
     dataset_x,
@@ -114,4 +129,22 @@ train_x, test_x, train_y, test_y = train_test_split(
     stratify   = dataset_y
 )
 
-print(train_x.shape, train_y.shape)
+print("Dataset successfully loaded.")
+
+model = LogisticRegression(max_iter = 5_000)
+model.fit(train_x, train_y)
+
+pred_y = model.predict(test_x)
+
+print(model.score(train_x, train_y))
+print(model.score(test_x,  test_y ))
+
+pass
+
+model = RandomForestClassifier(500)
+model.fit(train_x, train_y)
+
+pred_y = model.predict(test_x)
+
+print(model.score(train_x, train_y))
+print(model.score(test_x,  test_y ))
