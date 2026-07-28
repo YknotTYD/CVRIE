614c307870be04740fff89c65a588948a5453cc9
yknottyd <ethan.bertin-prevot@epitech.eu>
2026-07-28 20:34:35 +0200

    [main] Began the checklist - 'pass'.

--- main.py
+++ main.py
@@ -6,15 +6,25 @@ import pandas            as pd
 import matplotlib.pyplot as plt
 import kagglehub
 import os
+from   typing                  import Callable
 from   PIL                     import Image
 from   sklearn.model_selection import train_test_split
 from   sklearn.preprocessing   import StandardScaler
 from   sklearn.decomposition   import PCA
 from   sklearn.linear_model    import LogisticRegression
 from   sklearn.ensemble        import RandomForestClassifier
+from sklearn.model_selection   import cross_val_score
 
 # TODO: switch out PIL for something else
 # TODO: a logger
+
+# V The dataset only contains images and their associated labels. You may trim additional information.
+# V The overall dataset is representative of a classification issue.
+# ? You are able to justify your choice.( Choosing the same dataset as another group without justification is forbidden.)
+
+# V Your data could need to be normalised. Images of different sizes, color palettes or resolution might need  some pre-processing.
+# V Your data could be unbalanced. Are positive and negative cases both present and equally represented in your data?
+# V (X) Are there outliers that skew the data in specific directions or extremes?
 
 PENUMONIA = kagglehub.dataset_download("iamtapendu/rsna-pneumonia-processed-dataset")
 PENUMONIA_TRAIN_CSV    = PENUMONIA + "/stage2_train_metadata.csv"
@@ -61,19 +71,20 @@ def load_training_data(
 ) -> np.array:
 
     dataset_x = []
-    assert (load_len >= reduced_dimention)
+    assert (load_len >= reduced_dimention or load_len == -1)
 
     print("Loading dataset from source...")
 
     for i, patient in train_metadata.iterrows():
 
-        if (i >= load_len):
+        if (i >= load_len and load_len != -1):
             break
 
-        img = Image.open(f"{PNEUMONIA_TRAIN_IMAGES}/{patient.patientId}.png")
-        img = img.resize(img_size)
-
-        dataset_x.append(np.array(img).flatten())
+        dataset_x.append(
+            np.array(
+                Image.open(f"{PNEUMONIA_TRAIN_IMAGES}/{patient.patientId}.png").convert("L").resize(img_size)
+            ).flatten()
+        )
 
     print("Done.\n")
 
@@ -131,20 +142,71 @@ train_x, test_x, train_y, test_y = train_test_split(
 
 print("Dataset successfully loaded.")
 
-model = LogisticRegression(max_iter = 5_000)
-model.fit(train_x, train_y)
+def get_call_str(function_like: Callable, *args, **kwargs) -> str:
 
-pred_y = model.predict(test_x)
+    call_str = f"{function_like.__qualname__}("
 
-print(model.score(train_x, train_y))
-print(model.score(test_x,  test_y ))
+    if args:
+        call_str += "".join([str(arg) + ", " for arg in args])
 
-pass
+    if kwargs:
+        call_str += "".join(
+            [f"{key} = {value}, " for key, value in kwargs.items()]
+        )
 
-model = RandomForestClassifier(500)
-model.fit(train_x, train_y)
+    if args or kwargs:
+        call_str = call_str[:-2]
 
-pred_y = model.predict(test_x)
+    call_str += ")"
+    return call_str
 
-print(model.score(train_x, train_y))
-print(model.score(test_x,  test_y ))
+def test_model(model_class, *args, **kwargs) -> tuple[float, float]:
+
+    model = model_class(*args, **kwargs)
+    model.fit(train_x, train_y)
+
+    scores = cross_val_score(
+        model,
+        dataset_x, dataset_y.ravel(),
+        cv = 5, scoring = "f1"
+    )
+
+    return (scores.mean(), scores.std(), model)
+
+best_mean  = {"value": None, "model": None}
+best_std   = {"value": None, "model": None}
+
+def save_best(mean, std, model) -> None:
+
+    global best_mean
+    global best_std
+
+    if  best_mean["value"] is None or mean > best_mean["value"]:
+        best_mean["value"] = mean
+        best_mean["model"] = model
+
+    if  best_std["value"] is None or std < best_std["value"]:
+        best_std["value"] = std
+        best_std["model"] = model
+
+    return None
+
+#for i in range(10):
+#    mean, std, model = test_model(LogisticRegression, max_iter = 1_000 + i * 500)
+#    save_best(mean, std, model)
+#    print()
+#    break
+
+#for i in range(1, 11):
+#    for depth in list(range(1, 7)) + [None]:
+#
+#        mean, std, model = test_model(
+#            RandomForestClassifier,
+#            n_estimators = i * 50,
+#            max_depth = depth
+#        )
+#
+#        save_best(mean, std, model)
+#        print()
+
+print(test_model(RandomForestClassifier, n_estimators = 500, max_depth = 6))
